be444929225bcfa749783176e3c9b693f8cc6a39
pali08 <p.mikulaj@gmail.com>
2018-03-25 13:41:10 +0200

    tabtospace

--- compare_and_output.py
+++ compare_and_output.py
@@ -9,52 +9,52 @@ from draw_plot import draw_points
 import heapq
 
 def best_fit_find(best_fit, bcr_header, bcr_array, new_corsum_min, old_corsum_min, rots_count, pi_mult):
-	old_corsum_min = new_corsum_min
-	all_rots, cor_sums, matrices_of_diffs = align_matrices(best_fit, bcr_header, bcr_array, rots_count, pi_mult)
-	new_corsum_min_index = cor_sums.index(min(cor_sums))
-	new_corsum_min = min(cor_sums)
-	pi_mult = pi_mult/2
-	best_fit = all_rots[new_corsum_min_index]
-	return(best_fit, pi_mult, new_corsum_min, new_corsum_min_index, old_corsum_min, all_rots, cor_sums, matrices_of_diffs)
-	
+    old_corsum_min = new_corsum_min
+    all_rots, cor_sums, matrices_of_diffs = align_matrices(best_fit, bcr_header, bcr_array, rots_count, pi_mult)
+    new_corsum_min_index = cor_sums.index(min(cor_sums))
+    new_corsum_min = min(cor_sums)
+    pi_mult = pi_mult/2
+    best_fit = all_rots[new_corsum_min_index]
+    return(best_fit, pi_mult, new_corsum_min, new_corsum_min_index, old_corsum_min, all_rots, cor_sums, matrices_of_diffs)
+    
 def compare_and_output(infilename_pdb, infilename_bcr, rots_count, best_fits_count,project_name):
-	bcr_array = np.array(read_bcr_bin(infilename_bcr))
-	# we put some zeroes around 
-	#for i in range(0,5):
-		#bcr_array = np.insert(bcr_array,0, values=0, axis=0)
-		#bcr_array = np.insert(bcr_array,bcr_array.shape[0], values=0, axis=0)
-		#bcr_array = np.insert(bcr_array,0, values=0, axis=1)
-		#bcr_array = np.insert(bcr_array,bcr_array.shape[1], values=0, axis=1)
-	bcr_header = read_bcr_header(infilename_bcr)
-	if(bcr_header["xlength"] / bcr_header["xpixels"] -  bcr_header["ylength"] / bcr_header["ypixels"] < 0.01):
-		pixel_size = bcr_header["xlength"] / bcr_header["xpixels"]
-	else:
-		print("Pixel size has to be the same in x and y direction")
-		return(1)
-		sys.exit()
-	pi_mult = 2
-	coor_list = read_pdb(infilename_pdb)[1]
-	best_fit = coor_list # in first cykle, best fit is default rotation
-	list_of_all_rots = []
-	create_folder(project_name)
-	subfolder = create_subfolders(project_name, "textfiles")
-	subfolder_plot = create_subfolders(project_name, "graphs")
-	axisangles, cor_sums, diff_matrices, aligned_pdb_matrices = align_matrices(coor_list, bcr_header, bcr_array, rots_count, pi_mult)
-	#print(axisangles)
-	#print(cor_sums)
-	#print(len(diff_matrices))
-	best_fits = np.argsort(cor_sums)[::1][:best_fits_count]
-	#print(best_fits)
-	with open(os.path.join(subfolder, "{}_text_output.txt".format(project_name)), mode="w+", encoding='utf-8') as textoutput:
-		ind_best = 0
-		for i in range(0, len(best_fits)):
-			ind_best = best_fits[i]
-			textoutput.write("score: {} axis {} {} {} angle {} \n".format(cor_sums[ind_best],axisangles[ind_best][0],axisangles[ind_best][1],axisangles[ind_best][2],axisangles[ind_best][3])) 
-			max_point = np.amax(diff_matrices[ind_best])
-			min_point = np.amin(diff_matrices[ind_best])
-			avg = (max_point + min_point)/2
-			draw_points(diff_matrices[ind_best],i,subfolder_plot,cor_sums[ind_best], pixel_size, aligned_pdb_matrices[ind_best], bcr_array)
-	return(0)
+    bcr_array = np.array(read_bcr_bin(infilename_bcr))
+    # we put some zeroes around 
+    #for i in range(0,5):
+        #bcr_array = np.insert(bcr_array,0, values=0, axis=0)
+        #bcr_array = np.insert(bcr_array,bcr_array.shape[0], values=0, axis=0)
+        #bcr_array = np.insert(bcr_array,0, values=0, axis=1)
+        #bcr_array = np.insert(bcr_array,bcr_array.shape[1], values=0, axis=1)
+    bcr_header = read_bcr_header(infilename_bcr)
+    if(bcr_header["xlength"] / bcr_header["xpixels"] -  bcr_header["ylength"] / bcr_header["ypixels"] < 0.01):
+        pixel_size = bcr_header["xlength"] / bcr_header["xpixels"]
+    else:
+        print("Pixel size has to be the same in x and y direction")
+        return(1)
+        sys.exit()
+    pi_mult = 2
+    coor_list = read_pdb(infilename_pdb)[1]
+    best_fit = coor_list # in first cykle, best fit is default rotation
+    list_of_all_rots = []
+    create_folder(project_name)
+    subfolder = create_subfolders(project_name, "textfiles")
+    subfolder_plot = create_subfolders(project_name, "graphs")
+    axisangles, cor_sums, diff_matrices, aligned_pdb_matrices = align_matrices(coor_list, bcr_header, bcr_array, rots_count, pi_mult)
+    #print(axisangles)
+    #print(cor_sums)
+    #print(len(diff_matrices))
+    best_fits = np.argsort(cor_sums)[::1][:best_fits_count]
+    #print(best_fits)
+    with open(os.path.join(subfolder, "{}_text_output.txt".format(project_name)), mode="w+", encoding='utf-8') as textoutput:
+        ind_best = 0
+        for i in range(0, len(best_fits)):
+            ind_best = best_fits[i]
+            textoutput.write("score: {} axis {} {} {} angle {} \n".format(cor_sums[ind_best],axisangles[ind_best][0],axisangles[ind_best][1],axisangles[ind_best][2],axisangles[ind_best][3])) 
+            max_point = np.amax(diff_matrices[ind_best])
+            min_point = np.amin(diff_matrices[ind_best])
+            avg = (max_point + min_point)/2
+            draw_points(diff_matrices[ind_best],i,subfolder_plot,cor_sums[ind_best], pixel_size, aligned_pdb_matrices[ind_best], bcr_array)
+    return(0)
 
 
 #graphs_and_textfiles('input_files/1hzh.pdb','input_files/1012_1.bcr', 10, 'myprojectnm5') 
--- rotate.py
+++ rotate.py
@@ -8,47 +8,47 @@ import transform_coordinates
 import math
 '''
 def first_rot(x_rotations_count, y_rotations_count, z_rotations_count, pdb_file):
-	coord_list = read_pdb(pdb_file)[1] 
-	y_turn_angle = (2*np.pi)/y_rotations_count	
-	x_turn_angle = (2*np.pi)/x_rotations_count
-	z_turn_angle = (2*np.pi)/x_rotations_count
-	list_of_all_rots=[]
-	for i in range(0,x_rotations_count):
-		coor_list_x_rot = rotate(x_turn_angle*i,"1,0,0", *coord_list)
-		for j in range(0, y_rotations_count):
-			coor_list_y_rot = rotate(y_turn_angle*j, "0,1,0", *coor_list_x_rot)
-			for k in range(0, z_rotations_count):
-				coor_list_final_rot = rotate(z_turn_angle*k, "0,0,1", *coor_list_y_rot)
-				list_of_all_rots.append(coor_list_final_rot)
-				
-	return(list_of_all_rots)
+    coord_list = read_pdb(pdb_file)[1] 
+    y_turn_angle = (2*np.pi)/y_rotations_count  
+    x_turn_angle = (2*np.pi)/x_rotations_count
+    z_turn_angle = (2*np.pi)/x_rotations_count
+    list_of_all_rots=[]
+    for i in range(0,x_rotations_count):
+        coor_list_x_rot = rotate(x_turn_angle*i,"1,0,0", *coord_list)
+        for j in range(0, y_rotations_count):
+            coor_list_y_rot = rotate(y_turn_angle*j, "0,1,0", *coor_list_x_rot)
+            for k in range(0, z_rotations_count):
+                coor_list_final_rot = rotate(z_turn_angle*k, "0,0,1", *coor_list_y_rot)
+                list_of_all_rots.append(coor_list_final_rot)
+                
+    return(list_of_all_rots)
 '''
 def ranvec(pi_mult):
-	#coord_list = read_pdb(pdb_file)[1]
-	#list_of_all_rots = []
-	a = 2
-	while(a > 1.0):
-		xi1 = 1.0 - (2.0*random.random())
-		xi2 = 1.0 - (2.0*random.random())
-		a = (xi1*xi1) - (xi2*xi2)
-	b = 2.0*math.sqrt(1-a)
-	x_ranvec = xi1*b
-	y_ranvec = xi2*b
-	z_ranvec = 1.0 - (2.0*a)
-	ran_angle = random.random()*(pi_mult*math.pi)
-	return(x_ranvec, y_ranvec, z_ranvec, ran_angle)
-		
+    #coord_list = read_pdb(pdb_file)[1]
+    #list_of_all_rots = []
+    a = 2
+    while(a > 1.0):
+        xi1 = 1.0 - (2.0*random.random())
+        xi2 = 1.0 - (2.0*random.random())
+        a = (xi1*xi1) - (xi2*xi2)
+    b = 2.0*math.sqrt(1-a)
+    x_ranvec = xi1*b
+    y_ranvec = xi2*b
+    z_ranvec = 1.0 - (2.0*a)
+    ran_angle = random.random()*(pi_mult*math.pi)
+    return(x_ranvec, y_ranvec, z_ranvec, ran_angle)
+        
 def create_rots(rots_count, pi_mult, coor_list):
-	#coor_list = read_pdb(infilename)[1]
-	list_of_all_rots = []
-	list_of_all_axisangles = []
-	for i in range(0, rots_count):
-		ran_axisangle = ranvec(pi_mult) # it ll became axisangle
-		rotation = rotate(ran_axisangle, coor_list)
-		list_of_all_rots.append(rotation)
-		list_of_all_axisangles.append(ran_axisangle)
-	return(list_of_all_rots, list_of_all_axisangles)
-	
+    #coor_list = read_pdb(infilename)[1]
+    list_of_all_rots = []
+    list_of_all_axisangles = []
+    for i in range(0, rots_count):
+        ran_axisangle = ranvec(pi_mult) # it ll became axisangle
+        rotation = rotate(ran_axisangle, coor_list)
+        list_of_all_rots.append(rotation)
+        list_of_all_axisangles.append(ran_axisangle)
+    return(list_of_all_rots, list_of_all_axisangles)
+    
 #TODO 3 rotace -> 1 rotace (algoritmus s rovnomernou rotaciou vo v3etkych smeroch
 # na skusku: vsetky rotace do 1 grafu -> gula
 # nahodna rotace agt overit v nej systematicku rotaciu
